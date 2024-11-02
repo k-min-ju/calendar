ee1c13fc820c6d35fecf148cd299a31c969304b3
mjkim <alswn8065@gmail.com>
2024-11-02 14:46:19 +0900

    Set js doc

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -4,13 +4,8 @@ import { SpinnerProps, SpinnerTypeKeys } from '@/types';
 
 /**
  * https://www.davidhu.io/react-spinners/
- * @param {Object} props - The props object for the component.
+ * @param {Object} props - Props change for each spinner component.
  * @param props.spinnerType - Spinner type
- * @param props.size - Spinner size
- * @param props.color - Spinner color
- * @param props.loading - When true, spinner is displayed
- * @param props.cssOverride - Spinner css
- * @param props.speedMultiplier - Spinner rotation speed
  * @constructor
  */
 export default function Spinner<T extends SpinnerTypeKeys>({
