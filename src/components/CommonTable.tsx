e5fae857ee8432c593e79205d60c4279737d5048
mjkim <alswn8065@gmail.com>
2024-11-02 14:44:49 +0900

    Set type missing

--- src/components/CommonTable.tsx
+++ src/components/CommonTable.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { CommonTableProps, TableColumns, TableRows } from '@/types';
 
@@ -8,31 +9,37 @@ import { CommonTableProps, TableColumns, TableRows } from '@/types';
  * @param tableCellProps - TableCell Component Props
  * @constructor
  */
-export default function CommonTable<T extends object>({ columns, rows }: CommonTableProps<T>) {
+export default function CommonTable<T extends object>({ columns, rows }: CommonTableProps<T>): React.JSX.Element {
   return (
     <div className="table-container-wrapper">
       <TableContainer component={Paper} className="calendar-holiday-table">
         <Table>
           <TableHead>
             <TableRow>
-              {columns.map((column: TableColumns) => (
-                <TableCell key={column.label} {...column.tableCellProps}>
-                  {column.label}
-                </TableCell>
-              ))}
+              {columns.map(
+                (column: TableColumns): React.JSX.Element => (
+                  <TableCell key={column.label} {...column.tableCellProps}>
+                    {column.label}
+                  </TableCell>
+                )
+              )}
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((tableRows: TableRows<T>) =>
-              tableRows.data.map((data: T, index: number) => (
-                <TableRow key={JSON.stringify(data)}>
-                  {Object.keys(data).map(key => (
-                    <TableCell key={key} {...tableRows.tableCellProps?.[index]}>
-                      {data[key]}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
+            {rows.map((tableRows: TableRows<T>): React.JSX.Element[] =>
+              tableRows.data.map(
+                (data: T, index: number): React.JSX.Element => (
+                  <TableRow key={JSON.stringify(data)}>
+                    {Object.keys(data).map(
+                      (key: string): React.JSX.Element => (
+                        <TableCell key={key} {...tableRows.tableCellProps?.[index]}>
+                          {data[key]}
+                        </TableCell>
+                      )
+                    )}
+                  </TableRow>
+                )
+              )
             )}
           </TableBody>
         </Table>
